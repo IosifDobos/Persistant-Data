a46135bdcf93b2df5dd442bbdff63c621b7af36b
Iosif Dobos <c16735789@mydit.ie>
2017-04-14 16:00:41 +0100

    Update and rename read_mp3.c to read_MP3.c

--- read_MP3.c
+++ read_MP3.c
@@ -57,7 +57,7 @@ char *my_genre_array[80] = {
 //main function
 int main(int argc, char *argv[])
 {
-    //declare my variables and pointers
+    //declare my file pointer
     FILE *fp;
 
     //declare my structure variable
